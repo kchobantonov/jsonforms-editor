950b0de3498c0e2da8dc6426f5ac93921e0acf43
Krasimir Chobantonov <kchobantonov@gmail.com>
2025-12-07 14:35:26 -0500

    update to relative path

--- packages/app/webpack.config.js
+++ packages/app/webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].[contenthash].js',
     clean: true,
-    publicPath: process.env.PUBLIC_URL || '/',
+    publicPath: '',
   },
   devtool:
     process.env.NODE_ENV === 'production'
